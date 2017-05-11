710c5830ad92d315738d6e76d6417a5bdd901015
john.fu <john.fu@tripleonetech.net>
2017-05-11 13:30:23 +0800

    add utils including crypto and datetime

---
+++ src/utils/crypto.js
@@ -0,0 +1,12 @@
+const encrypt = (input, key) => {
+    return input
+}
+
+const decrypt = (input, key) => {
+    return input
+}
+
+module.exports = {
+    encrypt, 
+    decrypt,
+}
---
+++ src/utils/datetime.js
@@ -0,0 +1,15 @@
+require('date-utils')
+
+const format = (dt, diff) => {
+    if (!dt) {
+        dt = new Date()
+    } else {
+        dt = new Date(dt)
+    }
+    if (!diff) diff = 0
+    return new Date(dt.addSeconds(diff)).toFormat('YYYY-MM-DD HH24:MI:SS')
+}
+
+
+
+module.exports = { format }
