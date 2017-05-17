f158893548858552af04e4dd5416cf943a721de3
john.fu <john.fu@tripleonetech.net>
2017-05-17 10:41:35 +0800

    improve crypto function to buffer computer

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -3,15 +3,17 @@ const crypto = require('crypto');
 const algorithm = 'des-ecb'
 
 const encrypt = (input, key) => {
-	const cipher = crypto.createCipheriv(algorithm, key, '');
-	const output = cipher.update(input, 'utf8', 'base64');
+	const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), Buffer.alloc(0))
+	let output = cipher.update(input, 'utf8', 'base64');
+	output += cipher.final('base64');
 	return output
 }
 
 const decrypt = (input, key) => {
-	const decipher = crypto.createDecipheriv(algorithm, key, '')
-	let output = decipher.update(input, 'base64', 'utf8')
-	return output
+	const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), Buffer.alloc(0))
+	let output = decipher.update(input, 'base64', 'utf8');
+	output += decipher.final('utf8');
+	return output;
 }
 
 module.exports = {
