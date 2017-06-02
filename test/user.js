6d555bb16b54a7450923da06a025558e7942dd37
john.fu <john.fu@tripleonetech.net>
2017-06-02 10:52:54 +0800

    add test to signup

--- test/user.js
+++ test/user.js
@@ -11,16 +11,19 @@ global.config = config
 
 const username = 'test7777'
 const password = 'test7777'
-User.authorize(username, password, config.secret.token, (err, player) => {
-    if(!player) return
-    User.generateToken(player.id, config.timeout.token, (err, token) => {
-        console.log({
-            status: 'Ok',
-            message: 'Login successfully',
-            data: {
-                token,
-                ss: null,
-            }
-        })
-    })
-})
+
+User.signup(`testuser_${Date.now()}`, 'email.com', 'test1234', config.secret.token)
+
+// User.authorize(username, password, config.secret.token, (err, player) => {
+//     if(!player) return
+//     User.generateToken(player.id, config.timeout.token, (err, token) => {
+//         console.log({
+//             status: 'Ok',
+//             message: 'Login successfully',
+//             data: {
+//                 token,
+//                 ss: null,
+//             }
+//         })
+//     })
+// })
