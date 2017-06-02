17d6248e636643ff6a104a4c1f749543d4678a41
john.fu <john.fu@tripleonetech.net>
2017-06-02 14:45:43 +0800

    add test for user apis

---
+++ test/10_user.js
@@ -0,0 +1,65 @@
+const client = require('./lib/client')
+const player = {
+    username: `testuser_${Date.now()}`,
+    email: 'test@email.com',
+    password: 'pass1234',
+}
+
+describe('User', () => {
+    it('Get test user', (done) => {
+        client()
+        .get('/user/test')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+            should.not.exist(err)
+            res.body.should.have.property('user').and.equal('test')
+            done()
+        })
+    })
+    
+    it('Sign up', (done) => {
+        client()
+        .post('/user/signup')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .send(player)
+        .expect(201)
+        .end((err) => {
+            should.not.exist(err)
+            done()
+        })
+    })
+
+    it('Sign in', (done) => {
+        client()
+        .post('/user/login')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .send(player)
+        .expect(200)
+        .end((err, res) => {
+            should.not.exist(err)
+            res.body.should.have.property('token')
+            res.body.should.have.property('ssinfo')
+            done()
+        })
+    })
+
+    it('Login failed', (done) => {
+        client()
+        .post('/user/login')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .send({
+            username: 'wrong',
+            password: 'wrong',
+        })
+        .expect(401)
+        .end((err) => {
+            should.not.exist(err)
+            done()
+        })
+    })
+})
