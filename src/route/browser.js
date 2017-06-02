6ced15190f8089d0fc894fe1584e68b9f422a2f0
john.fu <john.fu@tripleonetech.net>
2017-06-02 14:44:25 +0800

    clean and clear code by eslint rules

--- src/route/browser.js
+++ src/route/browser.js
@@ -4,25 +4,25 @@ const path = require('path')
 const log4js = require('log4js')
 const utils = require('../utils')
 const uuidV4 = require('uuid/v4')
-const multer  = require('multer')
-const upload = multer({ dest: 'upload/' })
+const multer = require('multer')
 
+const upload = multer({ dest: 'upload/' })
 const logger = log4js.getLogger()
 
 module.exports = (route, config) => {
-    const getVersion = (req, res, next) => {
+    const getVersion = (req, res) => {
         const { platform, client } = req.query
         const version = Browser.getVersion(platform, client)
         return res.json(version)
     }
 
-    const createNewBrowser = async (req, res, next) => {
+    const createNewBrowser = async (req, res) => {
         try {
             const { client, homepage, company } = req.body
-            if (!client || !homepage || !company)  return res.status(400).send('Missing fields')
+            if (!client || !homepage || !company) return res.status(400).send('Missing fields')
             const { projectPath, version, legalCopyright } = config.browser
             const optionPath = path.join(projectPath, `src/clients/${client}`)
-            if (!fs.existsSync(optionPath))  fs.mkdirSync(optionPath)
+            if (!fs.existsSync(optionPath)) fs.mkdirSync(optionPath)
             if (req.file && req.file.path) {
                 utils.copy(req.file.path, path.join(optionPath, 'icon.ico'))
             }
@@ -40,14 +40,14 @@ module.exports = (route, config) => {
                 enabledProxy: false,
                 clientId: uuidV4().toUpperCase(),
                 proxyOptions: {
-                    localAddr: "127.0.0.1",
+                    localAddr: '127.0.0.1',
                     localPort: 21866,
-                    serverAddr: "106.75.147.144",
+                    serverAddr: '106.75.147.144',
                     serverPort: 19999,
-                    password: "0367E21094d36315",
-                    method: "aes-256-cfb",
-                    timeout: 180
-                }
+                    password: '0367E21094d36315',
+                    method: 'aes-256-cfb',
+                    timeout: 180,
+                },
             }
             // generate option file
             const optionFile = path.join(optionPath, 'client.json')
@@ -65,7 +65,7 @@ module.exports = (route, config) => {
                 icon,
             }
             await utils.copy(path.join(projectPath, 'dist/unpacked/electron.exe'), path.join(projectPath, 'dist/unpacked/safety-browser.exe'), { clobber: false })
-            await utils.rceditSync(path.join(projectPath, 'dist/unpacked/safety-browser.exe') , rceditOptions)
+            await utils.rceditSync(path.join(projectPath, 'dist/unpacked/safety-browser.exe'), rceditOptions)
 
             if (fs.existsSync(path.join(optionPath, 'default.pac'))) {
                 await utils.copy(path.join(optionPath, 'default.pac'), path.join(projectPath, 'src/app/config/default.pac'))
@@ -98,11 +98,11 @@ module.exports = (route, config) => {
         }
     }
 
-    const getCreateClientPage = (req, res, next) => {
-        return res.sendFile(path.join(__dirname, '..', 'public/client.html'));
+    const getCreateClientPage = (req, res) => {
+        return res.sendFile(path.join(__dirname, '..', 'public/client.html'))
     }
 
     route.get('/browser/version', getVersion)
     route.get('/browser/new', getCreateClientPage)
-    route.post('/browser/create', upload.single('icon'),  createNewBrowser)
+    route.post('/browser/create', upload.single('icon'), createNewBrowser)
 }
