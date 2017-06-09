00defdaa84d007dc04d63153eb4939035d5eefff
john.fu <john.fu@tripleonetech.net>
2017-06-09 18:13:15 +0800

    add timestamp columns

--- src/db/patches/1.sql
+++ src/db/patches/1.sql
@@ -1,5 +1,7 @@
 
 CREATE TABLE version (
     id serial,
-    ver int
+    ver int,
+    created timestamp DEFAULT CURRENT_TIMESTAMP,
+    last_updated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
--- src/db/patches/2.sql
+++ src/db/patches/2.sql
@@ -1,9 +1,10 @@
 
-
 CREATE TABLE player (
     id serial,
     username varchar(255),
     email varchar(255),
     password varchar(255),
-    verify boolean
+    verify boolean,
+    created timestamp DEFAULT CURRENT_TIMESTAMP,
+    last_updated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
