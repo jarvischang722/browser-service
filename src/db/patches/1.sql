a87087e91fe51c3c21718b98bca461c9edb508f6
john.fu <john.fu@tripleonetech.net>
2017-06-30 11:44:45 +0800

    modify db schema

--- src/db/patches/1.sql
+++ src/db/patches/1.sql
@@ -8,19 +8,34 @@ CREATE TABLE version (
 
 CREATE TABLE user (
     id serial,
-    username varchar(255),
-    email varchar(255),
+    username varchar(255) UNIQUE,
     password varchar(255),
+    salt varchar(255),
+    role int DEFAULT 1,
+    name varchar(255),
     status int DEFAULT 1,
+    parent bigint,
+    expire_in timestamp,
     created timestamp DEFAULT CURRENT_TIMESTAMP,
     last_updated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE user_mapping (
-    id serial,
-    userid int,
-    merchant varchar(255),
-    playerid int,
-    created timestamp DEFAULT CURRENT_TIMESTAMP,
-    last_updated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+INSERT INTO user (code, password, name)
+VALUES
+('tripleone', 'pass1234', '合众科技'),
+('agtop', 'pass1234', '澳门新葡京'),
+('hxpj', 'pass1234', '澳门新葡京'),
+('le18', 'pass1234', '乐赢'),
+('macaopj', 'pass1234', '澳门葡京'),
+('mgm', 'pass1234', '美高梅'),
+('twinbet', 'pass1234', 'Twinbet'),
+('xc33', 'pass1234', '新橙娱乐'),
+('xpj', 'pass1234', '新葡京'),
+('yuanbao361', 'pass1234', '元宝娱乐'),
+('wb88', 'pass1234', '万博娱乐城')
+;
+
+UPDATE user
+SET parent = 1
+WHERE id <> 1
+;
--- src/db/patches/2.sql
+++ src/db/patches/2.sql
@@ -1,6 +1,7 @@
 
 CREATE TABLE browser (
     id serial,
+    userid bigint,
     platform varchar(255),
     client varchar(255),
     version varchar(255),
@@ -10,30 +11,30 @@ CREATE TABLE browser (
     UNIQUE KEY `unique_browser` (`client`,`platform`)
 );
 
-INSERT INTO browser (platform, client, version, link)
+INSERT INTO browser (userid, platform, client, version, link)
 VALUES
-    ('windows', 'agtop', '2.8.88', 'http://www.agtop.t1t.games/safetybrowser-agtop-setup.exe'),
-    ('windows', 'hxpj', '2.8.88', 'http://www.hxpj.t1t.games/safetybrowser-hxpj-setup.exe'),
-    ('windows', 'le18', '2.8.88', 'http://www.le18.com/safetybrowser-le18-setup.exe'),
-    ('windows', 'macaopj', '2.9.0', 'http://www.p9601.com/safetybrowser-macaopj-setup.exe'),
-    ('windows', 'mgm', '2.8.88', 'http://www.9989234.com/safetybrowser-mgm-setup.exe'),
-    ('windows', 'tripleone', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/install.exe'),
-    ('windows', 'twinbet', '2.8.88', 'http://www.cn.twinbet.com/safetybrowser-twinbet-setup.exe'),
-    ('windows', 'xc33', '2.8.88', 'http://www.xc33.com/safetybrowser-xc33-setup.exe'),
-    ('windows', 'xpj', '2.8.88', 'http://www.xpj.t1t.games/safetybrowser-xpj-setup.exe'),
-    ('windows', 'yuanbao361', '2.9.0', 'http://www.demo.tripleonetech.com/safety-browser/safety-browser-yuanbao361-setup.exe'),
-    ('windows', 'wb88', '2.9.1', 'http://www.demo.tripleonetech.com/safety-browser/safety-browser-wb88-setup.exe'),
-    ('mac', 'le18', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/Le18.zip'),
-    ('mac', 'tripleone', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/Tripleonetech.zip'),
-    ('mac', 'twinbet', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/Twinbet.zip'),
-    ('mac', 'xc33', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/XC33.zip'),
-    ('android', 'agtop', '2.8.88', 'http://www.agtop.t1t.games/app-Agtop.apk'),
-    ('android', 'le18', '2.8.88', 'http://www.le18.com/app-Leyin.apk'),
-    ('android', 'macaopj', '2.8.88', 'http://www.macaopj.t1t.games/app-Macaopj.apk'),
-    ('android', 'mgm', '2.8.88', 'http://www.9989234.com/app-Mgm.apk'),
-    ('android', 'tripleone', '2.8.88', 'http://www.demo.tripleonetech.com/app-TripleOneTech1.apk'),
-    ('android', 'twinbet', '2.8.88', 'http://www.cn.twinbet.com/app-Twinbet.apk'),
-    ('android', 'xc33', '2.8.88', 'http://www.xc33.com/app-Xc33.apk'),
-    ('android', 'xpj', '2.8.88', 'http://www.xpj.t1t.games/app-Xpj.apk'),
-    ('android', 'wb88', '2.8.88', 'http://m.master.tripleonetech.net/app-Mwb88.apk')
+    (1, 'windows', 'tripleone', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/install.exe'),
+    (2, 'windows', 'agtop', '2.8.88', 'http://www.agtop.t1t.games/safetybrowser-agtop-setup.exe'),
+    (3, 'windows', 'hxpj', '2.8.88', 'http://www.hxpj.t1t.games/safetybrowser-hxpj-setup.exe'),
+    (4, 'windows', 'le18', '2.9.2', 'http://www.le18.com/safetybrowser-le18-setup.exe'),
+    (5, 'windows', 'macaopj', '2.9.2', 'http://www.p9601.com/safetybrowser-macaopj-setup.exe'),
+    (6, 'windows', 'mgm', '2.8.88', 'http://www.9989234.com/safetybrowser-mgm-setup.exe'),
+    (7, 'windows', 'twinbet', '2.8.88', 'http://www.cn.twinbet.com/safetybrowser-twinbet-setup.exe'),
+    (8, 'windows', 'xc33', '2.9.2', 'http://www.xc33.com/safetybrowser-xc33-setup.exe'),
+    (9, 'windows', 'xpj', '2.8.88', 'http://www.xpj.t1t.games/safetybrowser-xpj-setup.exe'),
+    (10, 'windows', 'yuanbao361', '2.9.2', 'http://www.demo.tripleonetech.com/safety-browser/safety-browser-yuanbao361-setup.exe'),
+    (11, 'windows', 'wb88', '2.9.1', 'http://www.demo.tripleonetech.com/safety-browser/safety-browser-wb88-setup.exe'),
+    (1, 'mac', 'tripleone', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/Tripleonetech.zip'),
+    (4, 'mac', 'le18', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/Le18.zip'),
+    (7, 'mac', 'twinbet', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/Twinbet.zip'),
+    (8, 'mac', 'xc33', '2.8.88', 'http://www.demo.tripleonetech.com/safety-browser/XC33.zip'),
+    (1, 'android', 'tripleone', '2.8.88', 'http://www.demo.tripleonetech.com/app-TripleOneTech1.apk'),
+    (2, 'android', 'agtop', '2.8.88', 'http://www.agtop.t1t.games/app-Agtop.apk'),
+    (4, 'android', 'le18', '2.8.88', 'http://www.le18.com/app-Leyin.apk'),
+    (5, 'android', 'macaopj', '2.8.88', 'http://www.macaopj.t1t.games/app-Macaopj.apk'),
+    (6, 'android', 'mgm', '2.8.88', 'http://www.9989234.com/app-Mgm.apk'),
+    (7, 'android', 'twinbet', '2.8.88', 'http://www.cn.twinbet.com/app-Twinbet.apk'),
+    (8, 'android', 'xc33', '2.8.88', 'http://www.xc33.com/app-Xc33.apk'),
+    (9, 'android', 'xpj', '2.8.88', 'http://www.xpj.t1t.games/app-Xpj.apk'),
+    (11, 'android', 'wb88', '2.8.88', 'http://m.master.tripleonetech.net/app-Mwb88.apk')
 ;
