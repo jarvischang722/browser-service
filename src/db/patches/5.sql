375842be7f19727db8e6ea2d98380fd367683e92
John Fu <john.js.tw@tripleonetech.net>
2018-03-12 16:22:38 +0800

    get long from db

--- src/db/patches/5.sql
+++ src/db/patches/5.sql
@@ -9,3 +9,19 @@ CREATE TABLE short (
   created timestamp DEFAULT CURRENT_TIMESTAMP,
   last_updated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+INSERT INTO short (
+  short, `long`, site_name, logo_url
+) VALUES 
+  ('t1t', 'www.tripleonetech.net', '合众科技', ''),
+  ('tot', 'www.tripleonetech.net', '合众科技', ''),
+  ('apple', 'www.apple.com', '苹果', ''),
+  ('lanhai', 'www.lanhai.t1t.games', '', ''),
+  ('xc', 'm.xc33.com', '新橙', 'http://52.198.79.141/download/icon/xc.png'),
+  ('youhu', 'm.youhu.t1t.games', '游虎娱乐', 'http://52.198.79.141/download/icon/youhu.png'),
+  ('macaopj', 'm.p9601.com', '澳门葡京', 'http://52.198.79.141/download/icon/macaopj.png'),
+  ('xpj', 'm.xpj.t1t.games', '新葡京', 'http://52.198.79.141/download/icon/xpj.png'),
+  ('lebo', 'm.lbbet888.com', '乐博', 'http://52.198.79.141/download/icon/lebo.png'),
+  ('lequ', 'm.lequ.t1t.games', '乐趣时时彩', 'http://52.198.79.141/download/icon/lequ.png'),
+  ('dlcity', 'm.dlcity.t1t.games', '电乐城', 'http://52.198.79.141/download/icon/dlcity.png')
+;
--- src/schema/short.js
+++ src/schema/short.js
@@ -1,81 +1,19 @@
-const getLong = (str) => {
-  let result = { long: '' }
-  switch (str) {
-    case 't1t':
-    case 'tot':
-      result = {
-        long: 'www.tripleonetech.net',
-        site_name: '合众科技',
-        logo_url: '',
-      }
-      break
-    case 'apple':
-      result = {
-        long: 'www.apple.com',
-        site_name: '苹果',
-        logo_url: '',
-      }
-      break
-    case 'lanhai':
-      result = {
-        long: 'www.lanhai.t1t.games',
-        site_name: '',
-        logo_url: '',
-      }
-      break
-    case 'xc':
-      result = {
-        long: 'm.xc33.com',
-        site_name: '新橙',
-        logo_url: 'http://52.198.79.141/download/icon/xc.png',
-      }
-      break
-    case 'youhu':
-      result = {
-        long: 'm.youhu.t1t.games',
-        site_name: '游虎娱乐',
-        logo_url: 'http://52.198.79.141/download/icon/youhu.png',
-      }
-      break
-    case 'macaopj':
-      result = {
-        long: 'm.p9601.com',
-        site_name: '澳门葡京',
-        logo_url: 'http://52.198.79.141/download/icon/macaopj.png',
-      }
-      break
-    case 'xpj':
-      result = {
-        long: 'm.xpj.t1t.games',
-        site_name: '新葡京',
-        logo_url: 'http://52.198.79.141/download/icon/xpj.png',
-      }
-      break
-    case 'lebo':
-      result = {
-        long: 'm.lbbet888.com',
-        site_name: '乐博',
-        logo_url: 'http://52.198.79.141/download/icon/lebo.png',
-      }
-      break
-    case 'lequ':
-      result = {
-        long: 'm.lequ.t1t.games',
-        site_name: '乐趣时时彩',
-        logo_url: 'http://52.198.79.141/download/icon/lequ.png',
-      }
-      break
-    case 'dlcity':
-      result = {
-        long: 'm.dlcity.t1t.games',
-        site_name: '电乐城',
-        logo_url: 'http://52.198.79.141/download/icon/dlcity.png',
-      }
-      break
-    default:
-      break
+const getLong = async (q) => {
+  const l = { long: '' }
+  if (!q) return l
+  const query = `
+    SELECT *
+    FROM short
+    WHERE short = ?
+    ;`
+  const results = await db.query(query, [q])
+  if (results.length <= 0) return l
+  const row = results[0]
+  return {
+    long: row.long,
+    site_name: row.site_name,
+    logo_url: row.logo_url,
   }
-  return result
 }
 
 module.exports = {
