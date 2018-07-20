deb03047fe5d5f5a6544843266663ac18f7f4677
Jun Chang <jun.js.tw@tripleonetech.net>
2018-07-20 12:48:50 +0800

    Code format

--- src/utils/shadowsocks.js
+++ src/utils/shadowsocks.js
@@ -5,25 +5,25 @@ const net = require('net')
  * @param {Object} ssConf : shadowsocks's configuration
  */
 const checkSSIsAvail = async (ssConf, config) => new Promise((resolve, reject) => {
-    try {
-        const socket = new net.Socket()
-        if (config.timeout) socket.setTimeout(config.timeout)
-        else socket.setTimeout(3000)
+  try {
+    const socket = new net.Socket()
+    if (config.timeout) socket.setTimeout(config.timeout)
+    else socket.setTimeout(3000)
 
-        socket.connect(ssConf.serverPort, ssConf.serverAddr, () => {
-            socket.destroy()
-            resolve(ssConf)
-        })
-        socket.on('timeout', () => {
-            socket.destroy()
-            resolve(false)
-        })
-        socket.on('error', (err) => {
-            resolve(false)
-        })
-    } catch (ex) {
-        throw ex
-    }
+    socket.connect(ssConf.serverPort, ssConf.serverAddr, () => {
+      socket.destroy()
+      resolve(ssConf)
+    })
+    socket.on('timeout', () => {
+      socket.destroy()
+      resolve(false)
+    })
+    socket.on('error', (err) => {
+      resolve(false)
+    })
+  } catch (ex) {
+    throw ex
+  }
 })
 
 
